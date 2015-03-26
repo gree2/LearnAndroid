a06d2d0a4749deeac375a3a1b414348424ddee05
hqlgree2 <hqlgree2@gmail.com>
2015-03-26 22:31:17 +0800

    build with error, but it can build anyway

--- PlayGif/app/src/main/java/com/example/hqlgree2/playgif/MainActivity.java
+++ PlayGif/app/src/main/java/com/example/hqlgree2/playgif/MainActivity.java
@@ -1,39 +1,61 @@
 package com.example.hqlgree2.playgif;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.StrictMode;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 
-
-public class MainActivity extends ActionBarActivity {
+/**
+ * Main activity, hosts the pager
+ *
+ * @author koral--
+ */
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((ViewPager) findViewById(R.id.main_pager)).setAdapter(new MainPagerAdapter(this));
     }
 
+    static class MainPagerAdapter extends FragmentStatePagerAdapter {
+        private final String[] mPageTitles;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public MainPagerAdapter(FragmentActivity act) {
+            super(act.getSupportFragmentManager());
+            mPageTitles = act.getResources().getStringArray(R.array.pages);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new GifSourcesFragment();
+                case 1:
+                    return new GifTextViewFragment();
+                case 2:
+                    return new GifTextureFragment();
+                case 3:
+                    return new AboutFragment();
+                default:
+                    throw new IndexOutOfBoundsException("Invalid page index");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return mPageTitles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mPageTitles[position];
+        }
     }
+
 }
