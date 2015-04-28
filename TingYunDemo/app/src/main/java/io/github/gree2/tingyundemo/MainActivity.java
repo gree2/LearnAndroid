b1d1c20995fe403f96486dd0a979fada0cfff6cc
hqlgree2 <hqlgree2@gmail.com>
2015-04-28 11:28:51 +0800

    add volley support

--- TingYunDemo/app/src/main/java/io/github/gree2/tingyundemo/MainActivity.java
+++ TingYunDemo/app/src/main/java/io/github/gree2/tingyundemo/MainActivity.java
@@ -2,19 +2,78 @@ package io.github.gree2.tingyundemo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
 import com.networkbench.agent.impl.NBSAppAgent;
 
+import io.github.gree2.tingyundemo.app.Const;
+import io.github.gree2.tingyundemo.app.AppController;
+
 public class MainActivity extends ActionBarActivity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         NBSAppAgent.setLicenseKey("56bf625a5c3f41dda247c7ca4e4b52f5").withLocationServiceEnabled(true).start(this);
-        NBSAppAgent.setLicenseKey("56bf625a5c3f41dda247c7ca4e4b52f5").start(this);
+        //NBSAppAgent.setLicenseKey("56bf625a5c3f41dda247c7ca4e4b52f5").start(this);
+
+        // findView
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.text_view);
+        // click
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSearch();
+            }
+        });
+    }
+
+    private void doSearch(){
+
+        final TextView tv = textView;
+        tv.setText("");
+        // request data
+        final StringRequest request = new StringRequest(Request.Method.GET, Const.URL_SEARCH + "android",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(final String response) {
+
+                        tv.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                tv.setText(response);
+                            }
+                        });
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+                    }
+                });
+        Log.d(TAG, request.getUrl());
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(request);
     }
 
     @Override
---
+++ TingYunDemo/app/src/main/java/io/github/gree2/tingyundemo/app/Const.java
@@ -0,0 +1,11 @@
+package io.github.gree2.tingyundemo.app;
+
+/**
+ * Created by Administrator on 2015-02-09.
+ */
+public class Const {
+
+    public static final String URL_BOOK = "http://it-ebooks-api.info/v1/book/";
+    public static final String URL_SEARCH = "http://it-ebooks-api.info/v1/search/";
+
+}
